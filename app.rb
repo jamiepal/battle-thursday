964bc4734f10731255ff7033c655bc5612b9a7fa
Charles Kettell <charleskettell@hotmail.co.uk>
2019-07-12 11:10:58 +0100

    player class intrdouced to Game class

--- app.rb
+++ app.rb
@@ -16,21 +16,20 @@ class Battle < Sinatra::Base
     end
 
     post '/names' do
-      # $game = Player.new(params[:player_1]).name
       $game = Game.new(params[:player_1], params[:player_2])
 
       redirect '/play'
     end
 
     get '/play' do
-      @player_1 = $game.player_1
-      @player_2 = $game.player_2
+      @player_1 = $game.player_1.name
+      @player_2 = $game.player_2.name
       erb(:play)
     end
 
     get '/attack' do
-      @player_1 = $game.player_1
-      @player_2 = $game.player_2
+      @player_1 = $game.player_1.name
+      @player_2 = $game.player_2.name
       erb(:attack)
     end
 
