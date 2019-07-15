2e0f82b8c694033702a11306d68ea68c97cf12a4
frank-carracedo <fjmc81@gmail.com>
2019-07-15 15:35:56 +0100

    fresh project

--- app.rb
+++ app.rb
@@ -1,39 +1,13 @@
 require 'capybara'
 require 'sinatra'
-require './lib/player.rb'
-require './lib/game.rb'
+# require './lib/bookmark.rb'
 
-class Battle < Sinatra::Base
+class Bookmark < Sinatra::Base
 
   enable :sessions
 
     get '/' do
-      erb(:intro)
-    end
-
-    get '/players' do
-      erb(:players)
-    end
-
-    post '/names' do
-      $game = Game.new(params[:player_1], params[:player_2])
-      redirect '/play'
-    end
-
-    get '/play' do
-      @player_1 = $game.player_1.name
-      @player_2 = $game.player_2.name
-      erb(:play)
-    end
-
-    get '/attack' do
-      $game.attack($game.opposing_player)
-      $game.switch_turns
-      if $game.game_over?
-        erb(:gameover)
-      else
-        erb(:attack)
-      end
+      erb(:index)
     end
 
     run! if app_file == $0
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,61 +1,6 @@
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Welcome to Battle. Your death awaits'
+    expect(page).to have_content 'Welcome Hello'
   end
 end
-
-feature 'play button' do
-  scenario 'play button re-directs you to player naming page' do
-    visit('/')
-    click_button 'Play!'
-    expect(page).to have_content 'Enter Player Names'
-  end
-end
-
-feature 'Entering players' do
-  scenario 'allows to enter names and be greeted on the following page' do
-    sign_in_and_play
-    expect(page).to have_content 'Richie vs. Charlie'
-  end
-end
-
-  feature 'See Player 2 hit points' do
-    scenario 'it allows Player 1 to see Player 2`s hit points' do
-      sign_in_and_play
-      expect(page).to have_content 'Charlie HP 100'
-    end
-  end
-
-  feature 'Attack Player 2' do
-    scenario 'it allows me to attack Player 2 and get conformation' do
-      sign_in_and_play
-      click_button 'Karate Chopskies'
-      expect(page).to have_content "You Karate Chopskied Charlie"
-    end
-  end
-
-  feature 'switching turns' do
-    scenario 'give the possibility to switch turns between players' do
-      sign_in_and_play
-      click_button 'Karate Chopskies'
-      click_button 'Karate Chopskies'
-      expect(page).to have_content "You Karate Chopskied Richie"
-    end
-  end
-
-  feature "Showing HP's of both players" do
-    scenario 'check above' do
-      sign_in_and_play
-      click_button 'Karate Chopskies'
-      expect(page).to have_content "Player 1: Richie: HP: 100"
-    end
-  end
-
-feature "ending the game" do
-  scenario "player 2 loses" do
-    sign_in_and_play
-    19.times {click_button "Karate Chopskies"}
-    expect(page).to have_content "Charlie, YOU LOSE!!"
-    end
-  end
